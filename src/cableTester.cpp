43267b0d5363cc6c755b07f8ca16bab4684900e3
duran tchuente <duran.tchuente@gmail.com>
2024-03-14 11:51:57 -0400

    definition verification method and compare array method

--- src/cableTester.cpp
+++ src/cableTester.cpp
@@ -11,18 +11,7 @@ int CableTester::orderPositionStraight[8] = {0, 1, 2, 3, 4, 5, 6, 7};//Definitio
 bool compareArrays(int a[], int b[], int n); //Definition du prototype de la fonction de comparaison de tableau
 
 void CableTester::verifyCableType(int resultOrderSignalPosition[], int n){ //Definition de la fonction de vérification du type de cable
-    if (compareArrays(orderPositionStraight, resultOrderSignalPosition, n)) //On verifie ici si le tableau qui resulte du teste est equivalent a celui du cable droit
-  {
-    Serial.println("Ce cable est droit");
-  }
-  else if (compareArrays(orderPositionCross, resultOrderSignalPosition, n)) //On verifie ici si le tableau qui resulte du teste est equivalent a celui du cable croisé
-  {
-    Serial.println("Ce cable est croisé");
-  }
-  else
-  {
-    Serial.println("Ce cable est inconnu");
-  }
+
 }
 
 bool compareArrays(int a[], int b[], int n) { //Implementation de la fonction de comparaison de tableaux
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,6 @@
 //Definition des variables et des tableaux contenant les broches utilisées par le programme
 char pinCable1[8] = {A4, A5, A6, 0, 1, 2, 3, 4};
 int pinCable2[8] = {5, 6, 7, 8, 9, 10, 11, 12};
-int responseArrayCable[8];
-CableTester cableTest;
 
 void setup() {
   //Definition des broches en entrées
@@ -23,17 +21,7 @@ void setup() {
 void loop() {
   int n = 0;
   do {  
-    digitalWrite(pinCable2[n], HIGH); //Envoie du signal élévé aux broches de sortie
-    for (int i = 0;i < 8;i++) {
-      if (digitalRead(pinCable1[i])) //Vérification de l'état du signal des broches d'entrées
-      {
-        responseArrayCable[n] = i; //Affectation des position des broches ayant recu un signagle élévé dans un tableau
-      }
-    }
-    digitalWrite(pinCable2[n], LOW); //Envoie du signal faible aux broches de sortie
-    n = n+1;
+    
   } while (n<8);
-  cableTest.verifyCableType(responseArrayCable, 8); //Appel de la fonction de verification du cable
-  
-  delay(2000);
+
 }
